cf0f4f00f25239e9e06f4712747776b86340f586
switch k <chukwuchinedu550@gmail.com>
2021-02-17 12:36:19 +0100

    Add files via upload

---
+++ database/migrations/2021_02_12_143645_create_cart_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->integer('checkout_id')->nullable();
+            $table->integer('product_id')->nullable();
+            $table->float('price');
+            $table->integer('quantity');
+            $table->float('total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
---
+++ database/migrations/2021_02_14_164647_create_check_out_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckOutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('check_out', function (Blueprint $table) {
+            $table->id();
+            $table->float('total');
+            $table->foreignId('user_id')
+                ->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('check_out');
+    }
+}
